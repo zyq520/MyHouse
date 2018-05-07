d1c646d8c5fea051728668ca3a1256d033802d5f
zhuangyq <zhuangyq@ngarihealth.com>
2018-05-07 15:37:08 +0800

    修改

--- src/main/java/com/house/App.java
+++ src/main/java/com/house/App.java
@@ -11,12 +11,13 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * @create 2018-04-23 上午 9:56
  **/
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(App.class);
-    }
+public class App{
+//public class App extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(App.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
